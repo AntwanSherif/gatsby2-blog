91596ba2afc0db022f83b6fcfc93f191f8503820
AntwanSherif <antwansherif@gmail.com>
2020-03-06 22:31:01 +0200

    Updates

--- src/components/common/Link.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link as GatsbyLink } from 'gatsby';
-import styles from './link.module.scss';
-
-export default function Link(props) {
-    return (
-        <GatsbyLink className={styles.link} {...props} />
-    )
-}
--- src/markdown/react.md
+++
@@ -1,9 +0,0 @@
----
-title: 'I <3 React!'
-date: '2020-03-03'
-image: 'https://source.unsplash.com/150x150/?reactjs'
-keywords: 'developers'
----
-
-# React!
-What a wonderful language!
